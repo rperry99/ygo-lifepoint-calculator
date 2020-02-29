59aee6cf0c9f11c087155d510f36cd5548c353e7
Russ Perry <rperry1886@gmail.com>
2020-02-28 19:01:26 -0500

    Add plus and minus functionality
    add the two click functions for plus and minus that will adjust the players score as necessary

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,51 @@
 $(document).ready(function() {
   let inputPoints = "";
   let action;
+  let player1Lp = 8000;
+  let player2Lp = 8000;
+
+  resetValues();
+
   $("#reset").click(() => {
-    $("#player1 .lp").html(8000);
-    $("#player2 .lp").html(8000);
-    $("#pointInput").val("");
-    inputPoints = "";
+    resetValues();
   });
+
   $("#clear").click(() => {
     $("#pointInput").val("");
     inputPoints = "";
   });
+
   $(".numButton").click(function() {
     if (inputPoints.length < 4) {
       inputPoints += $(this).html();
     }
     $("#pointInput").val(inputPoints);
   });
+
+  $(".minus").click(function() {
+    let player = $(this).attr("id");
+    if (player === "p1Minus") {
+      player1Lp -= parseInt(inputPoints);
+    } else if (player === "p2Minus") {
+      player2Lp -= parseInt(inputPoints);
+    }
+    resetValues();
+  });
+
+  $(".plus").click(function() {
+    let player = $(this).attr("id");
+    if (player === "p1Plus") {
+      player1Lp += parseInt(inputPoints);
+    } else if (player === "p2Plus") {
+      player2Lp += parseInt(inputPoints);
+    }
+    resetValues();
+  });
+
+  function resetValues() {
+    $("#player1 .lp").html(player1Lp);
+    $("#player2 .lp").html(player2Lp);
+    $("#pointInput").val("");
+    inputPoints = "";
+  }
 });
